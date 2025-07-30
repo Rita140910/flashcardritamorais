53f84c01f2a02f4f8a9bffb69df1d4c7f40405a9
Rita140910 <rita.oliveira.morais@escola.pr.gov.br>
2025-07-30 12:24:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cobras',
+    'Cobras são mamíferos, aves ou répteis??',
+    'Répteis.'
+)
+
+criaCartao(
+    'Onças',
+    ' Qual é o maior felino das Américas??',
+    ' A onça-pintada.'
+)
+
+criaCartao(
+    'Tubarões',
+    'Tubarões possuem ossos como nós?',
+    'Não, seu esqueleto é feito de cartilagem.'
+)
+
+criaCartao(
+    'Tigres',
+    'Qual é a característica mais distintiva da pelagem de um tigre??',
+    'Suas listras escuras.'
+)
